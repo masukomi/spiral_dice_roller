1554f97f50b5f0e2ff5532835e148af5a40658b6
masukomi <masukomi@masukomi.org>
2022-04-07 17:42:52 -0400

    service worker now supports offline caching

--- service_worker.js
+++ service_worker.js
@@ -1,9 +1,30 @@
+const cacheName = "spiralDiceRollerCache-v2";
+const assets = [
+    "spiral_dice_roller.html"
+];
+
 self.addEventListener('install', function() {
     console.log('Install!');
+    event.waitUntil(
+        caches
+            .open(cacheName)
+            .then(cache => {
+                return cache.addAll(assets);
+            })
+            .catch(err => console.log(err))
+    );
 });
 self.addEventListener("activate", event => {
     console.log('Activate!');
 });
-self.addEventListener('fetch', function(event) {
-    console.log('Fetch!', event.request);
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
 });
